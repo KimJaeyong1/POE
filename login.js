7f1252040b8b93dbceeb26319bd9ff1a24521b89
ShenWoo <95727730+KimJaeyong1@users.noreply.github.com>
2022-01-18 23:04:46 +0900

    POE Pan Page

--- login.js
+++ login.js
@@ -2,24 +2,28 @@ var saveID = "cat";
 var savePW = 1234;
 var inputID;
 var inputPW;
+var change = false;
 
 window.onload = function() {
     inputID = document.getElementById("input_id");
     inputPW = document.getElementById("input_pw");
+    change = document.getElementById("success");
 }
 
 function loginCheck() {
     if(inputID.value == saveID && inputPW.value == savePW) {
         loginSuccess();
-        alert("로그인 성공!")
+        alert(saveID+"님 환영 합니다!")
         console.log("로그인 성공")
-    } else {
-        alert("로그인 실패!")
+    } else if (inputID.value != saveID && inputPW.value == savePW) {
+        alert("등록된 아이디가 아닙니다.")
+        console.log("로그인 실패")
+    } else if (inputID.value == saveID && inputPW.value != savePW) {
+        alert("비밀번호가 틀렸습니다.")
         console.log("로그인 실패")
     }
 }
 
-
 function loginSuccess() {
-    location.href="index.html";
+    location.href="index2.html";
 }
