e2f65293ee39a8936d00ce073fafb26695fa19dc
ShenWoo <95727730+KimJaeyong1@users.noreply.github.com>
2022-01-22 17:37:51 +0900

    POE Pan Page

---
+++ div.js
@@ -0,0 +1,282 @@
+var list1;
+var list2;
+var list3;
+var list4;
+var list5;
+var list6;
+var list7;
+var list8;
+var list9;
+var list10;
+var list11;
+var list12;
+var main;
+
+list1 = document.getElementById("list1_content");
+list2 = document.getElementById("list2_content");
+list3 = document.getElementById("list3_content");
+list4 = document.getElementById("list4_content");
+list5 = document.getElementById("list5_content");
+list6 = document.getElementById("list6_content");
+list7 = document.getElementById("list7_content");
+list8 = document.getElementById("list8_content");
+list9 = document.getElementById("list9_content");
+list10 = document.getElementById("list10_content");
+list11 = document.getElementById("list11_content");
+list12 = document.getElementById("list12_content");
+main = document.getElementById("main_back");
+
+
+
+function listClick1() {
+    if(list1.style.display=="none") {
+        list1.style.display="block";
+        list2.style.display="none";
+        list3.style.display="none";
+        list4.style.display="none";
+        list5.style.display="none";
+        list6.style.display="none";
+        list7.style.display="none";
+        list8.style.display="none";
+        list9.style.display="none";
+        list10.style.display="none";
+        list11.style.display="none";
+        list12.style.display="none";
+        main.style.display="none";
+    } else {
+        list1.style.display="none";
+        main.style.display="block";
+    }
+};
+
+function listClick2() {
+    if(list2.style.display=="none") {
+        list1.style.display="none";
+        list2.style.display="block";
+        list3.style.display="none";
+        list4.style.display="none";
+        list5.style.display="none";
+        list6.style.display="none";
+        list7.style.display="none";
+        list8.style.display="none";
+        list9.style.display="none";
+        list10.style.display="none";
+        list11.style.display="none";
+        list12.style.display="none";
+        main.style.display="none";
+    } else {
+        list2.style.display="none";
+        main.style.display="block";
+    }
+};
+
+function listClick3() {
+    if(list3.style.display=="none") {
+        list1.style.display="none";
+        list2.style.display="none";
+        list3.style.display="block";
+        list4.style.display="none";
+        list5.style.display="none";
+        list6.style.display="none";
+        list7.style.display="none";
+        list8.style.display="none";
+        list9.style.display="none";
+        list10.style.display="none";
+        list11.style.display="none";
+        list12.style.display="none";
+        main.style.display="none";
+    } else {
+        list3.style.display="none";
+        main.style.display="block";
+    }
+};
+
+function listClick4() {
+    if(list4.style.display=="none") {
+        list1.style.display="none";
+        list2.style.display="none";
+        list3.style.display="none";
+        list4.style.display="block";
+        list5.style.display="none";
+        list6.style.display="none";
+        list7.style.display="none";
+        list8.style.display="none";
+        list9.style.display="none";
+        list10.style.display="none";
+        list11.style.display="none";
+        list12.style.display="none";
+        main.style.display="none";
+    } else {
+        list4.style.display="none";
+        main.style.display="block";
+    }
+};
+
+function listClick5() {
+    if(list5.style.display=="none") {
+        list1.style.display="none";
+        list2.style.display="none";
+        list3.style.display="none";
+        list4.style.display="none";
+        list5.style.display="block";
+        list6.style.display="none";
+        list7.style.display="none";
+        list8.style.display="none";
+        list9.style.display="none";
+        list10.style.display="none";
+        list11.style.display="none";
+        list12.style.display="none";
+        main.style.display="none";
+    } else {
+        list5.style.display="none";
+        main.style.display="block";
+    }
+};
+
+function listClick6() {
+    if(list6.style.display=="none") {
+        list1.style.display="none";
+        list2.style.display="none";
+        list3.style.display="none";
+        list4.style.display="none";
+        list5.style.display="none";
+        list6.style.display="block";
+        list7.style.display="none";
+        list8.style.display="none";
+        list9.style.display="none";
+        list10.style.display="none";
+        list11.style.display="none";
+        list12.style.display="none";
+        main.style.display="none";
+    } else {
+        list6.style.display="none";
+        main.style.display="block";
+    }
+};
+
+function listClick7() {
+    if(list7.style.display=="none") {
+        list1.style.display="none";
+        list2.style.display="none";
+        list3.style.display="none";
+        list4.style.display="none";
+        list5.style.display="none";
+        list6.style.display="none";
+        list7.style.display="block";
+        list8.style.display="none";
+        list9.style.display="none";
+        list10.style.display="none";
+        list11.style.display="none";
+        list12.style.display="none";
+        main.style.display="none";
+    } else {
+        list7.style.display="none";
+        main.style.display="block";
+    }
+};
+
+function listClick8() {
+    if(list8.style.display=="none") {
+        list1.style.display="none";
+        list2.style.display="none";
+        list3.style.display="none";
+        list4.style.display="none";
+        list5.style.display="none";
+        list6.style.display="none";
+        list7.style.display="none";
+        list8.style.display="block";
+        list9.style.display="none";
+        list10.style.display="none";
+        list11.style.display="none";
+        list12.style.display="none";
+        main.style.display="none";
+    } else {
+        list8.style.display="none";
+        main.style.display="block";
+    }
+};
+
+function listClick9() {
+    if(list9.style.display=="none") {
+        list1.style.display="none";
+        list2.style.display="none";
+        list3.style.display="none";
+        list4.style.display="none";
+        list5.style.display="none";
+        list6.style.display="none";
+        list7.style.display="none";
+        list8.style.display="none";
+        list9.style.display="block";
+        list10.style.display="none";
+        list11.style.display="none";
+        list12.style.display="none";
+        main.style.display="none";
+    } else {
+        list9.style.display="none";
+        main.style.display="block";
+    }
+};
+
+function listClick10() {
+    if(list10.style.display=="none") {
+        list1.style.display="none";
+        list2.style.display="none";
+        list3.style.display="none";
+        list4.style.display="none";
+        list5.style.display="none";
+        list6.style.display="none";
+        list7.style.display="none";
+        list8.style.display="none";
+        list9.style.display="none";
+        list10.style.display="block";
+        list11.style.display="none";
+        list12.style.display="none";
+        main.style.display="none";
+    } else {
+        list10.style.display="none";
+        main.style.display="block";
+    }
+};
+
+function listClick11() {
+    if(list11.style.display=="none") {
+        list1.style.display="none";
+        list2.style.display="none";
+        list3.style.display="none";
+        list4.style.display="none";
+        list5.style.display="none";
+        list6.style.display="none";
+        list7.style.display="none";
+        list8.style.display="none";
+        list9.style.display="none";
+        list10.style.display="none";
+        list11.style.display="block";
+        list12.style.display="none";
+        main.style.display="none";
+    } else {
+        list11.style.display="none";
+        main.style.display="block";
+    }
+};
+
+function listClick12() {
+    if(list12.style.display=="none") {
+        list1.style.display="none";
+        list2.style.display="none";
+        list3.style.display="none";
+        list4.style.display="none";
+        list5.style.display="none";
+        list6.style.display="none";
+        list7.style.display="none";
+        list8.style.display="none";
+        list9.style.display="none";
+        list10.style.display="none";
+        list11.style.display="none";
+        list12.style.display="block";
+        main.style.display="none";
+        main.style.display="none";
+    } else {
+        list12.style.display="none";
+        main.style.display="block";
+    }
+};
--- index.js
+++ index.js
@@ -1,38 +1,28 @@
-var mainContent;
+function showClock() {
+    var Clock = document.getElementById("time");
+    var currentDate = new Date();
+    var ampm = 'AM';
+    var currentHours = timeDisplay(currentDate.getHours(), 2);
+    var currentMinute = timeDisplay(currentDate.getMinutes(), 2);
+    var currentSeconds = timeDisplay(currentDate.getSeconds(), 2);
 
-window.onload = function() {
-    main1 = document.getElementById("main_contnet");
-    list1 = document.getElementById("total_list");
-}
-
-    
-function showClock()
-    {
-        var Clock = document.getElementById("time");
-        var currentDate = new Date();
-        var ampm = 'AM';
-        var currentHours = timeDisplay(currentDate.getHours(),2);
-        var currentMinute = timeDisplay(currentDate.getMinutes(),2);
-        var currentSeconds = timeDisplay(currentDate.getSeconds(),2);
-
-        if(currentHours >=  12) {
-            ampm = 'PM';
-            currentHours = timeDisplay(currentHours-12,2);
-        }
-
-        function timeDisplay(num, digit) {
-            var zero = '';
-            num = num.toString();
-            if(num.length < digit) {
-                for(i = 0; i < digit-num.length; i++) {
-                    zero += '0';
-                }
-            }
-            return zero + num;
-        }
-
-        Clock.innerHTML = ampm+" "+currentHours+" : "+currentMinute+" : "+currentSeconds;
-        
-        setTimeout(showClock,1000);
+    if (currentHours >= 12) {
+        ampm = 'PM';
+        currentHours = timeDisplay(currentHours - 12, 2);
     }
-    
+
+    function timeDisplay(num, digit) {
+        var zero = '';
+        num = num.toString();
+        if (num.length < digit) {
+            for (i = 0; i < digit - num.length; i++) {
+                zero += '0';
+            }
+        }
+        return zero + num;
+    }
+
+    Clock.innerHTML = ampm + " " + currentHours + " : " + currentMinute + " : " + currentSeconds;
+
+    setTimeout(showClock, 1000);
+}
